2ecc1f4542c5f43218fc78297c8b168cb8169cd4
Juan David Peña <juandavidp76@hotmail.com>
2023-03-24 14:21:11 -0500

    solucion a los ejercicios de js en el navegador

--- js-brow-2/todo.js
+++ js-brow-2/todo.js
@@ -1,24 +1,75 @@
+//variables
 const taskText = document.querySelector("#newTask"); //seleccion texto input
 const addTask = document.querySelector(".add"); //boton add
+const newT = document.querySelector(".tasks"); // seleccion div de tareas
 
-// const tareas = [];
-// let ident = 1;
-function handleSubmit() {
-    //crear checkbox
-    const newT = document.querySelector(".tasks");
-    const checkBox = document.createElement("input");
-    checkBox.type = "checkbox"
-    checkBox.name = "box";
-    checkBox.id = "box";
-    const label = document.createElement("label");
-    label.htmlFor = "box"
-    label.className = "tareas"
-    label.appendChild(document.createTextNode(taskText.value))
-    newT.appendChild(checkBox)
-    newT.appendChild(label);
-    // tareas.push({ id: ident, Title: taskText.value, completed: false });
-    // ident += 1;
-
+//checkbox and label attributes
+const tareas = [];
+let ident = 1;
+function handleSubmit(event) {
+  const checkBox = document.createElement("input"); //creacion input
+  const label = document.createElement("label"); //creacion label
+  const newDiv = document.createElement("div");
+  checkBox.type = "checkbox";
+  checkBox.name = "box";
+  checkBox.id = "box";
+  checkBox.className = "box";
+  label.htmlFor = "box";
+  label.className = "tareas completed";
+  label.appendChild(document.createTextNode(taskText.value));
+  newT.appendChild(newDiv);
+  newDiv.className = "tasks";
+  newDiv.appendChild(checkBox);
+  newDiv.appendChild(label);
+  tareas.push({ id: ident, Title: taskText.value, completed: false });
+  ident += 1;
+  event.preventDefault(); //avoid to refresh page
 }
 
-// console.log(tareas);
+//clean input field
+addTask.addEventListener("click", function () {
+  taskText.value = "";
+});
+
+//funcion de checkbox
+function handleCheckTask() {
+  let cont = 1;
+  const checkBox = document.getElementsByName("box"); //selecciona los checkbox
+  for (let j in checkBox) {
+    //itera sobre los checkbox
+    for (let i in tareas) {
+      //itera sobfre las objetos
+      if (tareas[i].id === cont && checkBox[j].checked) {
+        tareas[i].completed = true;
+      } else if (tareas[i].id === cont && checkBox[j].checked === false) {
+        tareas[i].completed = false;
+      }
+    }
+    cont += 1;
+  }
+}
+
+// funcion delete
+function handleDelete() {
+  const checkBox = document.getElementsByName("box");
+  const labels = document.getElementsByClassName("completed")
+  for (let i in checkBox){
+    for (let j in labels){
+  if(checkBox[i].checked){
+    checkBox[i].remove()
+    labels[i].remove()
+  }
+}
+}
+}
+//funcion tachado
+function completed(e) {
+    if (e.target.type === "checkbox") {
+      e.target.parentNode.classList.toggle("is-completed") //muestra u oculta la propiedad
+    }
+  }
+const divG = document.querySelector("#task")
+  divG.addEventListener("click", function(e) {
+    completed(e)
+  })
+
