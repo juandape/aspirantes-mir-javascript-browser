91ffee93f5a5486d33610fd0f1d65b7377dbbf9e
Juan David Peña <juandavidp76@hotmail.com>
2023-03-29 12:40:27 -0500

    actualizacion2

--- js-brow-4/code.js
+++ js-brow-4/code.js
@@ -66,6 +66,7 @@ const menu = [
   },
 ];
 const createul = document.createElement("ul")
+createul.className = "list"
 document.body.appendChild(createul)
 
 function list(menu){
@@ -73,6 +74,9 @@ function list(menu){
   const createli = document.createElement("li")
   const createli2 = document.createElement("li")
   const createli3 = document.createElement("li")
+  createli.className = "name"
+  createli2.className = "link"
+  createli3.className = "items"
   createli.textContent = menu[i].name
   createli2.textContent = menu[i].link
   createul.appendChild(createli)
@@ -84,52 +88,9 @@ function list(menu){
   else{
     // createli3.appendChild(createul)
     createli3.textContent = list(menu[i].items)
-    // createul.appendChild(createli3)
   }
   }
   
 }
 
-
 document.body.appendChild(list(menu))
-
-// const lista = document.querySelector("ul");
-
-// for (let i in menu) {
-//   const elementn = document.createElement("li");
-//   elementn.textContent = menu[i].name;
-//   lista.appendChild(elementn);
-
-//   const elementl = document.createElement("li");
-//   elementl.textContent = menu[i].link;
-//   lista.appendChild(elementl);
-//   //items vacios
-//   if (menu[i].items.length === 0) {
-//     const elementi = document.createElement("li");
-//     elementi.textContent = menu[i].items;
-//     lista.appendChild(elementi);
-//     //items con elementos
-//   } else {
-//     const elementul = document.createElement("ul");
-//     lista.appendChild(elementul);
-
-//     for (let j in menu[i].items) {
-//       const elementi1 = document.createElement("li");
-//       elementi1.textContent = menu[i].items[j].name;
-//       elementul.appendChild(elementi1);
-
-//       const elementl1 = document.createElement("li");
-//       elementl1.textContent = menu[i].items[j].link;
-//       elementul.appendChild(elementl1);
-
-//       if (menu[i].items[j].items.length === 0) {
-//         const elementt1 = document.createElement("li");
-//         elementt1.textContent = menu[i].items[j].items;
-//         elementul.appendChild(elementt1);
-//       } else {
-//         const elementul2 = document.createElement("ul");
-//         elementul2.appendChild(elementt1);
-//       }
-//     }
-//   }
-// }
