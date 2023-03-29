5e6f71aae9db8dde46510e3e883fd571b706ded1
Juan David Peña <juandavidp76@hotmail.com>
2023-03-29 17:22:20 -0500

    cambio de color

--- js-brow-4/code.js
+++ js-brow-4/code.js
@@ -65,16 +65,18 @@ const menu = [
     ],
   },
 ];
-// document.body.appendChild(createul);
 
+
+
+//creacion de lista
 function list(menu) {
   const createul = document.createElement("ul");
   createul.className = "list";
   for (let i in menu) {
     const createli = document.createElement("li");
     const createli2 = document.createElement("li");
-    createli.className = "name";
-    createli2.className = "link";
+    createli.className = "name elemento";
+    createli2.className = "link elemento";
     createli.textContent = menu[i].name;
     createli2.textContent = menu[i].link;
     createul.appendChild(createli);
@@ -87,3 +89,12 @@ function list(menu) {
 }
 
 document.body.appendChild(list(menu));
+
+
+//cambio de color
+let change = document.querySelectorAll("li")
+for (let i=0; i<change.length; i++){
+change[i].addEventListener("click", function(){
+  change[i].classList.toggle("name--ocultar")
+} )
+}
